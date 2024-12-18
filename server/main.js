1bbf41b27bc5b12148bf10be53af657ddcedc005
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-12-18 22:53:49 +0800

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,25 +1,3 @@
 import { Meteor } from 'meteor/meteor';
-import { WebApp } from 'meteor/webapp';
 import '../imports/startup/server';
-import '../imports/api/logs';
-import './methods';
-
-// 设置环境变量
-const FILE_PATH = process.env.FILE_PATH || Assets.absoluteFilePath('tmp');
-const PORT = process.env.PORT || 3000;
-
-// 创建临时目录
-if (!fs.existsSync(FILE_PATH)) {
-  fs.mkdirSync(FILE_PATH, { recursive: true });
-  console.log(`${FILE_PATH} is created`);
-}
-
-// 设置基本路由
-WebApp.connectHandlers.use('/', (req, res) => {
-  res.writeHead(200);
-  res.end('Hello world!');
-});
-
-Meteor.startup(() => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+import '../imports/api/methods';
