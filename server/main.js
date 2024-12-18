c6ef64270dd3b9f5266927d8ef7be58a3cc3934f
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-12-18 22:55:35 +0800

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,3 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 import '../imports/startup/server';
 import '../imports/api/methods';
+
+Meteor.startup(() => {
+  // 服务器启动时的初始化代码
+  console.log(`Server is running on port: ${process.env.PORT || 3000}`);
+});
