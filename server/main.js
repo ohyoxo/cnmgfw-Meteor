dbcde741a77f79c34ed9f6cf432a4577247cb464
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-02-28 17:53:09 +0800

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,16 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import { WebApp } from 'meteor/webapp';
-import '../imports/api/logs';
-import '../imports/startup/server/file-handler';
+import '../imports/api/websocket-server.js';
 
 Meteor.startup(() => {
-  // 设置基础路由
-  WebApp.rawConnectHandlers.use('/', (req, res, next) => {
-    if (req.url === '/') {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello world!');
-    } else {
-      next();
-    }
-  });
+  console.log('Meteor server has started');
 });
