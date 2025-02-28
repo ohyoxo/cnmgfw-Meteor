14032fc037f9904ee22606641b779b0d37a0e9ac
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-02-28 17:53:31 +0800

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,10 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
-  const root = createRoot(container);
-  root.render(<App />);
+  console.log('Client has started');
 });
